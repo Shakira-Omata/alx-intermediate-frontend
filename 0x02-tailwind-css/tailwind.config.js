9234f1479dda82219326d4e87f7d45c37adaee5f
Shakira Omata <osajipurity3@gmail.com>
2025-10-30 13:04:17 +0300

    t1

--- 0x02-tailwind-css/tailwind.config.js
+++ 0x02-tailwind-css/tailwind.config.js
@@ -1,8 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  // This tells Tailwind to scan all HTML and JS files inside the 'src' directory
-  // for Tailwind class names so it can generate the necessary CSS.
-  content: ["./src/**/*.{html,js}"], 
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
